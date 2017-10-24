d26a4fa44b3d802f864a09d9347cd1fcaca62eb0
FaizalSanyo <faizal.sanyo@gmail.com>
2017-10-24 16:43:56 +0200

    Personal Information - DONE -

---
+++ Pillars/Person.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pillars
+{
+    class Person
+    {
+
+
+        // Creating Private Fields
+        private
+            int age;
+            string fName;
+            string lName;
+            double height;
+            float weight;
+
+
+
+        // Creating Public properties for person class's objects
+
+        // a public property for age
+
+        public int Age // age attribute
+        {
+
+            set
+            {
+                age = value;
+            }
+            get
+            {
+                return age;
+            }
+        }
+
+        // a public property for fName
+        public string FirstName
+        {
+
+            set
+            {
+                fName = value;
+            }
+            get
+            {
+                return fName;
+            }
+        }
+
+        // a public property for lName
+        public string LastName
+        {
+
+            set
+            {
+                lName = value;
+            }
+            get
+            {
+                return lName;
+            }
+        }
+
+        // a public property for height
+        public double Height
+        {
+
+            set
+            {
+                height = value;
+            }
+            get
+            {
+                return height;
+            }
+        }
+
+        // a public property for weight
+        public float Weight
+        {
+
+            set
+            {
+                weight = value;
+            }
+            get
+            {
+                return weight;
+            }
+        }
+
+
+
+    }
+}
--- Pillars/Program.cs
+++ Pillars/Program.cs
@@ -8,8 +8,19 @@ namespace Pillars
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            // QUESTION 1 --  NO YOU CAN'T DIRECTLY ACCESS THE VARIABLES(Private Fields of Person Class) 
+            // BUT YOU CAN ACCESS THE PUPLIC PROPERTY OF THAT FIELD(VARIABLE)
+
+            PersonHandler ShowThePersonalInfo = new PersonHandler();
+            ShowThePersonalInfo.UserInfo();
+       
+
         }
+
+        
+
     }
 }
+
